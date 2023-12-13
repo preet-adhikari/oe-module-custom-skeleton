e719f7f2bdf282dbae1c06a3dfb743976e0a0048
Preet Adhikari <blazin.preet@gmail.com>
2023-12-13 10:30:08 +0545

    Created the deserializer for DeviceRequest

--- src/FhirDeviceRequestSerializer.php
+++ src/FhirDeviceRequestSerializer.php
@@ -10,6 +10,12 @@
 namespace OpenEMR\Modules\FHIRDeviceRequest;
 
 use OpenEMR\FHIR\R4\FHIRDomainResource\FHIRDeviceRequest;
+use OpenEMR\FHIR\R4\FHIRElement\FHIRAnnotation;
+use OpenEMR\FHIR\R4\FHIRElement\FHIRCodeableConcept;
+use OpenEMR\FHIR\R4\FHIRElement\FHIRCoding;
+use OpenEMR\FHIR\R4\FHIRElement\FHIRIdentifier;
+use OpenEMR\FHIR\R4\FHIRElement\FHIRInstant;
+use OpenEMR\FHIR\R4\FHIRElement\FHIRUri;
 
 class FhirDeviceRequestSerializer
 {
@@ -20,6 +26,65 @@ class FhirDeviceRequestSerializer
 
     public static function deserialize($fhirJson)
     {
-        //Creating the deserializer
+        $identifiers = $fhirJson['identifier'] ?? [];
+        $instantiatesUris = $fhirJson['instantiatesUri'] ?? [];
+        $reasonCode = $fhirJson['reasonCode'] ?? [];
+        $note = $fhirJson['note'] ?? [];
+
+
+        unset($fhirJson['identifier']);
+        unset($fhirJson['instantiatesUri']);
+        unset($fhirJson['reasonCode']);
+        unset($fhirJson['note']);
+
+
+        $deviceRequest = new FHIRDeviceRequest($fhirJson);
+
+
+        foreach ($identifiers as $item)
+        {
+            $obj = new FHIRIdentifier($item);
+            $type = $item['type'] ?? [];
+            $coding = $type['coding'] ?? [];
+            unset($type['coding']);
+            $codeableConcept = new FHIRCodeableConcept($type);
+            foreach($coding as $codingItem)
+            {
+                $codingItem = new FHIRCoding($codingItem);
+                $codeableConcept->addCoding($codingItem);
+            }
+            $obj->setType($codeableConcept);
+            $deviceRequest->addIdentifier($obj);
+        }
+
+        foreach($instantiatesUris as $item)
+        {
+            $obj = new FHIRUri($item);
+            $deviceRequest->addInstantiatesUri($obj);
+        }
+
+        foreach($reasonCode as $item)
+        {
+            $coding = $item['coding'] ?? [];
+            $text = $item['text'] ?? [];
+            unset($item['coding']);
+            unset($item['text']);
+            $codeableConcept = new FHIRCodeableConcept($item);
+            foreach ($coding as $codingItem)
+            {
+                $codingItem = new FHIRCoding($codingItem);
+                $codeableConcept->addCoding($codingItem);
+            }
+            $codeableConcept->setText($text);
+            $deviceRequest->addReasonCode($codeableConcept);
+        }
+
+        foreach($note as $item)
+        {
+            $obj = new FHIRAnnotation($item);
+            $deviceRequest->addNote($obj);
+        }
+
+        return $deviceRequest;
     }
 }
